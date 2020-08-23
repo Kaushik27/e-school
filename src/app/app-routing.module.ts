406386d666d82a9fb673d48b1089ebea847c459b
Kaushik Mandal <kaushikmandal27@gmail.com>
2020-08-23 23:43:57 +0530

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 // const routes: Routes = [{
 //   path: '', 
-//   component: DemoComponent
+//   component: HomeComponent
 // },
 // {
 //   path: '', 
