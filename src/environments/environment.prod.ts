3e26978e823e00e444dee0f06e1323a3a71736e2
Kaushik Mandal <kaushikmandal27@gmail.com>
2020-09-02 01:53:47 +0530

    firebase configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDIRu9fywrp94YmvFgZutxpWa1PHJMzifg",
+    authDomain: "e-school-d13ac.firebaseapp.com",
+    databaseURL: "https://e-school-d13ac.firebaseio.com",
+    projectId: "e-school-d13ac",
+    storageBucket: "e-school-d13ac.appspot.com",
+    messagingSenderId: "477743472054",
+    appId: "1:477743472054:web:a9ab4f9cacf113d9e27517",
+    measurementId: "G-462QWYZSG0"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDIRu9fywrp94YmvFgZutxpWa1PHJMzifg",
+    authDomain: "e-school-d13ac.firebaseapp.com",
+    databaseURL: "https://e-school-d13ac.firebaseio.com",
+    projectId: "e-school-d13ac",
+    storageBucket: "e-school-d13ac.appspot.com",
+    messagingSenderId: "477743472054",
+    appId: "1:477743472054:web:a9ab4f9cacf113d9e27517",
+    measurementId: "G-462QWYZSG0"
+  }
 };
 
 /*
