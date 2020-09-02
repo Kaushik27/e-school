de4deb19c3448a7879c4289f43e93f00c113b15d
Kaushik Mandal <kaushikmandal27@gmail.com>
2020-09-03 03:14:06 +0530

    commented unimplemented methods

--- src/app/data/database/student-db.service.ts
+++ src/app/data/database/student-db.service.ts
@@ -15,23 +15,23 @@ export class StudentService {
     this.studentsRef = db.list(this.dbPath);
   }
  
-  createStudent(customer: Student): void {
-    this.studentsRef.push(customer);
-  }
+//   createStudent(customer: Student): void {
+//     this.studentsRef.push(customer);
+//   }
  
-  updateStudent(key: string, value: any): Promise<void> {
-    return this.studentsRef.update(key, value);
-  }
+//   updateStudent(key: string, value: any): Promise<void> {
+//     return this.studentsRef.update(key, value);
+//   }
  
-  deleteStudent(key: string): Promise<void> {
-    return this.studentsRef.remove(key);
-  }
+//   deleteStudent(key: string): Promise<void> {
+//     return this.studentsRef.remove(key);
+//   }
  
   getStudentsList(): AngularFireList<Student> {
     return this.studentsRef;
   }
  
-  deleteAll(): Promise<void> {
-    return this.studentsRef.remove();
-  }
+//   deleteAll(): Promise<void> {
+//     return this.studentsRef.remove();
+//   }
 }
